aef91af207085f284cc79ddf492cde9b0c54690a
Fred88n7 <79079144+Fred88n7@users.noreply.github.com>
2022-02-24 14:17:07 +0100

    font/farve

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#6A110A",
+      white: "#fff",
+    },
+    fontFamily: {
+      h1: ["Archivo Black"],
+      h2: ["Prata"],
+      body: ["Quicksand"],
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
