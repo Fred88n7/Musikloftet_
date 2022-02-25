bc4018f071d64f6ceadd54fec7ca2ea7db00523e
Fred88n7 <79079144+Fred88n7@users.noreply.github.com>
2022-02-25 08:07:16 +0100

    hej

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     extend: {},
     colors: {
       primary: "#6A110A",
+      dark: "#550E08",
       white: "#fff",
+      hover: "#FF0019",
     },
     fontFamily: {
       h1: ["Archivo Black"],
